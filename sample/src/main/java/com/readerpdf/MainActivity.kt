2f8be8d4a83af918f77e8331bbc6bc92af084845
Agustin Dominguez <agustin.dominguez.lopez1988@gmail.com>
2023-01-18 13:01:35 +0100

    load pdf page and reset view

--- sample/src/main/java/com/readerpdf/MainActivity.kt
+++ sample/src/main/java/com/readerpdf/MainActivity.kt
@@ -2,12 +2,15 @@ package com.readerpdf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.readerpdf.databinding.ActivityMainBinding
 import com.artifex.mupdf.fitz.Buffer
 import com.artifex.mupdf.fitz.PDFDocument
 import com.artifex.mupdf.fitz.Rect
 import com.viewer.PDFCore
 import com.viewer.PageAdapter
+import com.viewer.PageView
+import com.viewer.ReaderView
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private var adapter: PageAdapter? = null
     private var core: PDFCore? = null
     lateinit var document: PDFDocument
+    private var actualPage = 0
+    private val pwd = "q82n3ks92j9sd72bnsldf7823hbzx7"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -87,10 +92,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadPdfPage(){
+        addNewPage()
 
+        binding.readerView.applyToChildArround(object : ReaderView.ViewMapper() {
+            override fun applyToView(view: View) {
+                (view as PageView).apply {
+                    update()
+                }
+            }
+        }, actualPage)
+
+        actualPage++
+        binding.loadNewPage.text = "Ld page $actualPage"
+    }
+
+    private fun addNewPage() {
+        val documentFile = File(inernalAssetsFolder, "/Main.pdf")
+        val insert = "${actualPage.toString().padStart(6, '0')}.pdf"
+        val pageFile = File("${cacheDir.absolutePath}/assets", insert)
+        val pageToInsert = PDFDocument.openDocument(pageFile.absolutePath) as PDFDocument
+        pageToInsert.authenticatePassword(pwd)
+        document.graftPage(actualPage, pageToInsert, 0)
+        document.deletePage(actualPage + 1)
+        adapter?.removePageSize(actualPage)
+        val links = pageToInsert.loadPage(0)?.links
+
+        if (links != null) {
+            core?.addPageLinks(actualPage, links)
+            adapter?.setLinks(links, actualPage)
+
+            binding.readerView.applyToChild(object : ReaderView.ViewMapper() {
+                override fun applyToView(view: View) {
+                    (view as PageView).apply {
+                        updateLinks(links)
+                    }
+                }
+            }, actualPage)
+        }
+
+        document.save(documentFile.absolutePath, "")
     }
 
     private fun reset(){
+        binding.readerView.applyToChildren(object : ReaderView.ViewMapper() {
+            override fun applyToView(view: View) {
+                (view as PageView).releaseBitmaps()
+            }
+        })
 
+        adapter?.reset()
+
+        binding.readerView.destroy()
+        core?.onDestroy()
     }
 }
