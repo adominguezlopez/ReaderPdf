4229c7ac885828c5fd83b095db293000d33edbac
Agustin Dominguez <agustin.dominguez.lopez1988@gmail.com>
2023-01-18 12:06:50 +0100

    added reader viewer lib and pdf empty generation

--- sample/src/main/java/com/readerPdf/MainActivity.kt
+++ sample/src/main/java/com/readerPdf/MainActivity.kt
@@ -3,15 +3,24 @@ package com.ReaderPdf
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ReaderPdf.databinding.ActivityMainBinding
+import com.artifex.mupdf.fitz.Buffer
+import com.artifex.mupdf.fitz.PDFDocument
+import com.artifex.mupdf.fitz.Rect
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private lateinit var inernalAssetsFolder: String
+    private val totalPages = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        inernalAssetsFolder = applicationContext.cacheDir!!.absolutePath+"/assets/"
 
         binding.downloadAssetsButton.setOnClickListener {
             copyAssets(this)
@@ -35,7 +44,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clearPdf(){
+        val documentFile = File(inernalAssetsFolder, "/Main.pdf")
 
+        val document = PDFDocument()
+
+        val buffer = Buffer()
+
+        for (i in 0 until totalPages) {
+            val page = document.addPage(
+                Rect(0f, 0f, 6f, 8f),
+                0,
+                document.newDictionary(),
+                buffer
+            )
+            document.insertPage(-1, page)
+        }
+        buffer.destroy()
+
+        document.save(documentFile.absolutePath, "")
     }
 
     private fun loadPdf(){
